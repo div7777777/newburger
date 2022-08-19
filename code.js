de4b6fefa09b0ff751daeed6cadcfc8fc0ab439f
div7777777 <99870561+div7777777@users.noreply.github.com>
2022-08-19 20:12:21 +0100

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+        // menu burgeur
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  // 
+          // changeur de thème
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
